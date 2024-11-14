23fadb9481a226250632d73fa18e0fca0abd5797
admin <admin@LAPTOP-1E6C53B4>
2024-11-14 12:59:56 +0700

    delete

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -2,15 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBpa8frZEBf55O5AAjjc5r-wJU1GjJUuIw",
-  authDomain: "apartment-fee-manager.firebaseapp.com",
-  projectId: "apartment-fee-manager",
-  storageBucket: "apartment-fee-manager.firebasestorage.app",
-  messagingSenderId: "607548746873",
-  appId: "1:607548746873:web:9cf3d04f9f43afa744f8a8",
-  measurementId: "G-Y0ME6HX49E"
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
